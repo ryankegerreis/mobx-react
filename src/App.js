f05d9050122f0dbccdb83f50897862a4dc051890
Ryan Kegerreis <Rkegerreis@assemblysoftware.com>
2019-12-03 09:40:26 -0500

    Ejecting

--- src/App.js
+++ src/App.js
@@ -3,24 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Whoa
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div className='App'>Mobx Test</div>;
 }
 
 export default App;
